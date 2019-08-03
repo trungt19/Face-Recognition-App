555d073a45d22f9b523ea80fcfb73a27c46e0e75
Trung Thai <trungthai@trungs-mbp.lan>
2019-08-02 17:21:53 -0700

    added ability to add photo and Clarifai API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
-import './App.css';
+import Clarifai from 'clarifai';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '72cd3858f1584cba99fa51dbdef6c4a8'
+});
 
 const particlesOptions ={
   particles: {
@@ -22,22 +28,38 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-
+      imageUrl: ''
     }
   }
 onInputChange = (event) => {
-  console.log(event);
+  this.setState({input: event.target.value});
+}
+
+onButtonSubmit = () => {
+  this.setState({imageUrl: this.state.input});
+  app.models.predict(
+    Clarifai.FACE_DETECT_MODEL,
+    this.state.input)
+    .then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // There was an error
+      }
+  );
 }
 
   render() {
     return (
       <div className="App">
       <Particles className='particles'
-                params={particlesOptions} />
+        params={particlesOptions} />
         <Navigation />
         <Logo /> 
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     )
   }
