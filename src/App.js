edb84ae24c66bd890e96ea6b6dd2a0eddb6a022c
Trung Thai <trungthai@trungs-mbp.lan>
2019-08-08 15:23:07 -0700

    Connected the front end to the back end

--- src/App.js
+++ src/App.js
@@ -33,9 +33,29 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+      }
+    })
+  }
+
+ 
 
 calculateFaceLocation = (data) => {
   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -52,7 +72,6 @@ calculateFaceLocation = (data) => {
 }
 
 displayFaceBox = (box) => {
-  console.log(box);
   this.setState({box: box})
 }
 
@@ -66,8 +85,23 @@ onButtonSubmit = () => {
     .predict(
     Clarifai.FACE_DETECT_MODEL,
     this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-    .catch(err => console.log(err));
+    .then(response => {
+      if(response) {
+        fetch('http://localhost:5000/image', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          id: this.state.user.id
+        }) 
+      })
+      .then(response => response.json())
+      .then(count => {
+        this.setState(Object.assign(this.state.user, { entries: count}))
+      })
+     }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log('did not fetch api'));
 }
 
 onRouteChange = (route) => {
@@ -84,12 +118,13 @@ onRouteChange = (route) => {
     return (
       <div className="App">
       <Particles className='particles'
-        params={particlesOptions} />
+        params={particlesOptions} 
+        />
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
         { route === 'home' 
         ? <div>
          <Logo /> 
-         <Rank />
+         <Rank name={this.state.user.name} entries={this.state.user.entries} />
          <ImageLinkForm 
          onInputChange={this.onInputChange} 
          onButtonSubmit={this.onButtonSubmit}
@@ -98,12 +133,12 @@ onRouteChange = (route) => {
         </div>
         : (
           route === 'signin' 
-          ? <Signin onRouteChange={this.onRouteChange}/>
-          : <Register onRouteChange={this.onRouteChange}/>
+          ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
         )
         }
       </div>
-    )
+    );
   }
 }
 
